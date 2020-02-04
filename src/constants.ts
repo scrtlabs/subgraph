0d0ff3bb20c85a3af0d44025f9be1a1b93a07da8
Victor Grau Serrat <lacabra@users.noreply.github.com>
2020-02-04 19:01:08 +0000

    MAINT: updated addresses for testnet v2

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 let keyManagementAddresses = new Array<string>()
-keyManagementAddresses.push('0x08f1a8d4c3085839315ff3da1acf194697864855')
+keyManagementAddresses.push('0x19585BcDf99D8a4507710F4cf290DB2ccEC27833')
 
 export let KEY_MANAGEMENT_ADDRESSES = keyManagementAddresses
